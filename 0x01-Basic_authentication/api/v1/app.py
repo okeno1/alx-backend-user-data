ef91e8ccef02ea8d39822e6bb155a7c70dbbebd4
Othino <micahothino44@gmail.com>
2023-05-30 09:53:16 -0700

    error handler: unauthorized

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,7 +7,6 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 
 from api.v1.views import app_views
-from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 
 
