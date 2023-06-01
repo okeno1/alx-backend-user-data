e03efd1c0390221af0f64057a9818e6a27854919
Othino <micahothino44@gmail.com>
2023-06-01 06:06:05 -0700

    sessions in database

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,59 +1,63 @@
 #!/usr/bin/env python3
 """
-Define class SessionDButh
+create a new authentication system, based on Session ID stored in database
 """
-from .session_exp_auth import SessionExpAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
 
 
 class SessionDBAuth(SessionExpAuth):
     """
-    Definition of SessionDBAuth class that persists session data
-    in a database
+    contains methods that handles creating
+    a new authentication system, based on Session ID stored in database
     """
 
     def create_session(self, user_id=None):
         """
-        Create a Session ID for a user_id
-        Args:
-           user_id (str): user id
+        creates a session id and saves to a file
         """
-        session_id = super().create_session(user_id)
-        if not session_id:
+        self.session_id = super().create_session(user_id)
+        if not self.session_id:
             return None
-        kw = {
-            "user_id": user_id,
-            "session_id": session_id
-        }
-        user = UserSession(**kw)
-        user.save()
-        return session_id
+        user_session_inf = {'session_id': self.session_id, 'user_id': user_id}
+        user_session = UserSession(**user_session_inf)
+        user_session.save()
+        return self.session_id
 
     def user_id_for_session_id(self, session_id=None):
         """
-        Returns a user ID based on a session ID
-        Args:
-            session_id (str): session ID
-        Return:
-            user id or None if session_id is None or not a string
+        returns the User ID by requesting UserSession in the
+        database based on session_id
         """
-        user_id = UserSession.search({"session_id": session_id})
-        if user_id:
-            return user_id
+
+        if not session_id:
+            return None
+
+        UserSession.load_from_file()
+        sessions = UserSession.search({'session_id': session_id})
+
+        if not sessions:
+            return None
+
+        for session in sessions:
+            return session.user_id
         return None
 
     def destroy_session(self, request=None):
         """
-        Destroy a UserSession instance based on a
-        Session ID from a request cookie
+        destroys the UserSession based on the Session ID from the request
+        cookie
         """
-        if request is None:
+        if not request:
             return False
-        session_id = self.session_cookie(request)
-        if not session_id:
+        self.session_id = self.session_cookie(request)
+        if not self.session_id:
             return False
-        user_session = UserSession.search({"session_id": session_id})
-        if user_session:
-            user_session[0].remove()
+        UserSession.load_from_file()
+        sessions_present = UserSession.search({'session_id': self.session_id})
+        if not sessions_present:
+            return False
+        for session in sessions_present:
+            del session
             return True
         return False
--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-""" UserSession module
+"""
+class for a User session
 """
 from models.base import Base
 
 
 class UserSession(Base):
     """
-    UserSession class
+    contains methods that manages a user's session and session ids
     """
 
     def __init__(self, *args: list, **kwargs: dict):
         """
-        Initialize a UserSession instance
         """
         super().__init__(*args, **kwargs)
         self.user_id = kwargs.get('user_id')
